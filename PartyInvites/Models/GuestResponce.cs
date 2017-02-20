06bc08365b95eeeff4827e0caefafa05a017f3ea
Аркадій <arkasha9@gmail.com>
2017-02-20 17:35:42 +0200

    Add project files.

---
+++ PartyInvites/Models/GuestResponce.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace PartyInvites.Models
+{
+    public class GuestResponce
+    {
+        [Required(ErrorMessage = "Вы не ввели имя")]
+        public  string Name { get; set; }
+        [Required(ErrorMessage = "Введите адрес електронной почты")]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage = "Вы ввели не коректный адрес електронной почты")]
+        public  string Email { get; set; }
+        [Required(ErrorMessage = "Введите телефон")]
+        public  string Phone { get; set; }
+        [Required(ErrorMessage = "Укажите примите ли Вы участие в вечеринке")]
+        public  bool? WillAttend { get; set; }
+    }
+}
